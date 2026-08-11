1eb86bbb7f045d7eac68571090ba0aab6959a60b
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-12 02:46:00 +0700

    Update build.gradle.kts

--- MovieBox/build.gradle.kts
+++ MovieBox/build.gradle.kts
@@ -1,1 +1,15 @@
+version = 1
 
+cloudstream {
+    language = "id"
+    description = "🎥 Movies & TV Series"
+    authors = listOf("Noritoshi")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Anime",
+        "AsianDrama",
+    )
+}
