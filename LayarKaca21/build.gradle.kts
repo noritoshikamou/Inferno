108c9b26f22c4fc34462bbc9bd81ffb54f101eca
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-11 20:36:21 +0700

    Update build.gradle.kts

--- LayarKaca21/build.gradle.kts
+++ LayarKaca21/build.gradle.kts
@@ -1,1 +1,22 @@
+// use an integer for version numbers
+version = 1
 
+cloudstream {
+    description = "🎬 LayarKaca21"
+    language = "id"
+    authors = listOf("Dzikri")
+
+    status = 1
+
+    tvTypes =
+        listOf(
+            "AsianDrama",
+            "TvSeries",
+            "Movie",
+            "Anime",
+        )
+
+    iconUrl = "https://i.ibb.co.com/vCtZLCQf/LAYAR21.png"
+
+    isCrossPlatform = false
+}
