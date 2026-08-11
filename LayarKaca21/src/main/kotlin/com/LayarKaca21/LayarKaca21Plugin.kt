6ac0819f25962da8eaa1aeaf4512e57f282a00ac
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-12 01:01:05 +0700

    Update LayarKaca21Plugin.kt

--- LayarKaca21/src/main/kotlin/com/LayarKaca21/LayarKaca21Plugin.kt
+++ LayarKaca21/src/main/kotlin/com/LayarKaca21/LayarKaca21Plugin.kt
@@ -7,10 +7,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class LayarKaca21Plugin : BasePlugin() {
     override fun load() {
-        // Mendaftarkan API Utama LayarKaca21
         registerMainAPI(LayarKaca21())
-        
-        // Mendaftarkan Ekstraktor Kustom agar terhubung
-        registerExtractorAPI(LayarKaca21PlayerPage())
+        registerExtractorAPI(Lk21PlayerPage())
     }
 }
