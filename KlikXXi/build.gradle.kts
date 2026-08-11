1cbec9bd1f32ce758fc7054eb1d7b911d380bbe2
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-12 03:35:35 +0700

    Update build.gradle.kts

--- KlikXXi/build.gradle.kts
+++ KlikXXi/build.gradle.kts
@@ -1,1 +1,13 @@
+version = 1
 
+cloudstream {
+    description = "KlikXXi - Streaming Movie and TV Series"
+    language = "id"
+    authors = listOf("🎥 Movies & TV Series")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "AsianDrama",
+    )
+}
