096f71d54c7e1ca3627436fb1228c4e3dd2ba6b4
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-12 04:38:47 +0700

    Update build.gradle.kts

--- Ngefilm/build.gradle.kts
+++ Ngefilm/build.gradle.kts
@@ -1,1 +1,21 @@
+// use an integer for version numbers
+version = 1
 
+cloudstream {
+    description = "🎥 Movies & TV Series"
+    language = "id"
+    authors = listOf("Noritoshi")
+
+    status = 1
+
+    tvTypes =
+        listOf(
+            "AsianDrama",
+            "TvSeries",
+            "Movie",
+        )
+
+    iconUrl = "https://i.ibb.co.com/67bx3Vnw/Ngefilm.png"
+
+    isCrossPlatform = false
+}
