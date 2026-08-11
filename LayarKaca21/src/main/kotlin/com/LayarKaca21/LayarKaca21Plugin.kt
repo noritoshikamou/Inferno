ca542b3ec94b1f4f35d04aabf844fd1967bdca1c
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-11 20:45:22 +0700

    Update LayarKaca21Plugin.kt

--- LayarKaca21/src/main/kotlin/com/LayarKaca21/LayarKaca21Plugin.kt
+++ LayarKaca21/src/main/kotlin/com/LayarKaca21/LayarKaca21Plugin.kt
@@ -1,19 +1,13 @@
-package com.lk21
+package com.LayarKaca21
 
-// Mengimpor konteks Android dan komponen plugin bawaan Cloudstream
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
-// Anotasi penanda bahwa kelas ini adalah titik masuk utama (entry point) untuk plugin Cloudstream
 @CloudstreamPlugin
-class Lk21Plugin : Plugin() {
-    
-    // Fungsi utama yang dipanggil saat plugin pertama kali dimuat oleh aplikasi Cloudstream
+class LayarKaca21Plugin : Plugin() {
     override fun load(context: Context) {
-        // Mendaftarkan API utama LK21 yang sudah kita buat
-        registerMainAPI(Lk21Provider())
-        
-        // Catatan: Karena kita belum pakai extractor kustom, baris registerExtractorAPI di bawah bisa dilewati/dihapus dulu.
+        // Sesuaikan dengan nama kelas provider utama di file LayarKaca21.kt Anda
+        registerMainAPI(LayarKaca21())
     }
 }
