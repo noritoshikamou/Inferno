666ea32756b5ff04039eb798a63c21c11c29944d
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-12 14:31:13 +0700

    Update Idlix.kt

--- Idlix/src/main/kotlin/com/Idlix/Idlix.kt
+++ Idlix/src/main/kotlin/com/Idlix/Idlix.kt
@@ -283,4 +283,26 @@ class Idlix : MainAPI() {
         }
         return true
     }
+
+    // Deklarasi data class untuk menghindari error Unresolved reference di Extractor/Idlix
+    data class ResponseSource(
+        @JsonProperty("hls") val hls: Boolean,
+        @JsonProperty("videoSource") val videoSource: String,
+        @JsonProperty("securedLink") val securedLink: String?,
+    )
+
+    data class Tracks(
+        @JsonProperty("kind") val kind: String?,
+        @JsonProperty("file") val file: String,
+        @JsonProperty("label") val label: String?,
+    )
+
+    data class ResponseHash(
+        @JsonProperty("embed_url") val embed_url: String,
+        @JsonProperty("key") val key: String,
+    )
+
+    data class AesData(
+        @JsonProperty("m") val m: String,
+    )
 }
