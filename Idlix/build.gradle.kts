d89b44c429483a35c72f52e8046cf04849c72829
noritoshikamou <noritoshi.kamou@gmail.com>
2026-08-12 11:01:42 +0700

    Update build.gradle.kts

--- Idlix/build.gradle.kts
+++ Idlix/build.gradle.kts
@@ -1,1 +1,22 @@
+// use an integer for version numbers
+version = 1
 
+cloudstream {
+    description = "🎥 Movies & TV Series"
+    language = "id"
+    authors = listOf("Noritoshi")
+
+    status = 1
+
+    tvTypes =
+        listOf(
+            "AsianDrama",
+            "TvSeries",
+            "Movie",
+            "Anime",
+        )
+
+    iconUrl = "https://i.ibb.co.com/VcG8HmWj/Idlix.png"
+
+    isCrossPlatform = false
+}
